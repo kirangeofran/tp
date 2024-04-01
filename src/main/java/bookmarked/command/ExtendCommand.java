e02325c8b53f6a3f90469836ab36c69d360202f1
kirangeofran <kirangeofran123@gmail.com>
2024-04-02 02:16:51 +0800

    Changed extend command to be more readable + added javadocs for Borrow,Extend,Return

--- src/main/java/bookmarked/command/ExtendCommand.java
+++ src/main/java/bookmarked/command/ExtendCommand.java
@@ -1,33 +1,81 @@
 package bookmarked.command;
 
 import bookmarked.Book;
-import java.io.File;
-
 import bookmarked.storage.BookStorage;
+import bookmarked.ui.Ui;
+import bookmarked.exceptions.BookNotFoundException;
+import bookmarked.exceptions.BookNotBorrowedException;
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Arrays;
 
-
+/**
+ * The {@code ExtendCommand} class handles the extension of the borrowing period for a book.
+ */
 public class ExtendCommand extends Command {
-    private String bookName;
+    private String[] commandParts;
     private ArrayList<Book> listOfBooks;
     private File bookDataFile;
 
-    public ExtendCommand(String bookName, ArrayList<Book> listOfBooks, File bookDataFile) {
-        this.bookName = bookName;
+    /**
+     * Constructs an {@code ExtendCommand} with the specified command parts, list of books, and file.
+     *
+     * @param commandParts the parts of the command input, including the book name
+     * @param listOfBooks  the current list of books
+     * @param bookDataFile the file where the list of books is stored
+     */
+    public ExtendCommand(String[] commandParts, ArrayList<Book> listOfBooks, File bookDataFile) {
+        this.commandParts = commandParts;
         this.listOfBooks = listOfBooks;
         this.bookDataFile = bookDataFile;
     }
 
+    /**
+     * Executes the extend command by trying to extend the due date of a borrowed book.
+     * It catches specific exceptions and delegates error messages to the {@code Ui} class.
+     */
     @Override
     public void handleCommand() {
+        try {
+            runExtendCommand();
+            BookStorage.writeBookToTxt(bookDataFile, listOfBooks);
+        } catch (BookNotFoundException e) {
+            Ui.printBookNotFoundExceptionMessage(); // Call the method without parameters
+        } catch (BookNotBorrowedException e) {
+            Ui.printBookNotBorrowedExceptionMessage(); // Call the method without parameters
+        }
+    }
+
+    /**
+     * Runs the logic to extend the due date of a borrowed book. If the book is not found
+     * or is not currently borrowed, it throws corresponding exceptions.
+     *
+     * @throws BookNotFoundException      if the book name provided does not match any book
+     * @throws BookNotBorrowedException   if the book found is not currently borrowed
+     */
+    private void runExtendCommand() throws BookNotFoundException, BookNotBorrowedException {
+        if (commandParts.length <= 1) {
+            throw new BookNotFoundException();
+        }
+
+        String bookNameToExtend = String.join(" ", Arrays.copyOfRange(commandParts, 1, commandParts.length));
+        boolean bookFound = false;
+
         for (Book book : listOfBooks) {
-            if (book.getName().equalsIgnoreCase(bookName) && book.getIsBorrowed()) {
-                book.extendDueDate(); // extend due date by 7 days
-                System.out.println("Due date for " + bookName + " has been extended by a week.");
-                BookStorage.writeBookToTxt(bookDataFile, listOfBooks); // Save the updated due date
-                return;
+            if (book.getName().equalsIgnoreCase(bookNameToExtend)) {
+                bookFound = true;
+                if (book.getIsBorrowed()) {
+                    book.extendDueDate(); // Make sure this method exists in your Book class
+                    System.out.println("Due date for '" + bookNameToExtend + "' has been extended by a week.");
+                    return;
+                } else {
+                    throw new BookNotBorrowedException();
+                }
             }
         }
-        System.out.println("Book not found or not borrowed: " + bookName);
+        if (!bookFound) {
+            throw new BookNotFoundException();
+        }
     }
+
 }
--- src/main/java/bookmarked/command/ReturnCommand.java
+++ src/main/java/bookmarked/command/ReturnCommand.java
@@ -10,10 +10,24 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * Handles the "return" command from the user.
+ * When executed, it returns the book with the given name from the list of books,
+ * marking it as not borrowed. Assumes that the book name is passed in the commandParts array.
+ */
 public class ReturnCommand extends Command {
     private String bookName;
     private ArrayList<Book> listOfBooks;
     private File bookDataFile;
+
+    /**
+     * Constructs a ReturnCommand object.
+     *
+     * @param commandParts An array of strings, where the first element is the command,
+     *                     and the subsequent elements are the parts of the book name.
+     * @param listOfBooks  The list of books from which a book will be returned.
+     * @param bookDataFile The data file where books are stored.
+     */
     public ReturnCommand(String[] commandParts, ArrayList<Book> listOfBooks, File bookDataFile) {
         assert listOfBooks != null : "list of books should not be empty";
         assert commandParts != null : "commandParts should not be null";
@@ -23,6 +37,11 @@ public class ReturnCommand extends Command {
         this.bookDataFile = bookDataFile;
     }
 
+    /**
+     * Executes the "return" command.
+     * Filters the list of books for any books that match the given book name and marks them as returned.
+     * Updates the book data file with the changes.
+     */
     @Override
     public void handleCommand() {
         // Filter the list for books that match the name
@@ -39,6 +58,13 @@ public class ReturnCommand extends Command {
         }
     }
 
+    /**
+     * Marks the books found in the list with the name provided as returned.
+     * If the book is not found, prints an error message.
+     *
+     * @param foundBooks The list of books with names that match the book to be returned.
+     * @throws EmptyListException If the list of books is empty.
+     */
     public void runReturnCommand(List<Book> foundBooks) throws EmptyListException {
         if (this.listOfBooks.isEmpty()) {
             throw new EmptyListException();
