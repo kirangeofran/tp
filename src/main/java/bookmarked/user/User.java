26853d903ccc330e41205d84e5ab63cb6418329d
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-04-13 00:04:50 +0800

    Modify User to allow feature to extend due date in User

--- src/main/java/bookmarked/user/User.java
+++ src/main/java/bookmarked/user/User.java
@@ -8,27 +8,28 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class User {
-    private String description;
+    private static final int EXTENSION_DAYS = 7;
+    private String userName;
     private ArrayList<Book> listOfBooks;
     private ArrayList<UserBook> listOfUserBooks;
     private ArrayList<Integer> userBooksIndex;
 
-    public User(String description, ArrayList<Book> listOfBooks) {
-        this.description = description;
+    public User(String userName, ArrayList<Book> listOfBooks) {
+        this.userName = userName;
         this.userBooksIndex = new ArrayList<>();
         this.listOfBooks = listOfBooks;
         this.listOfUserBooks = new ArrayList<>();
     }
 
     public String getName() {
-        return this.description;
+        return this.userName;
     }
 
     public ArrayList<Book> getUserBooks() {
         ArrayList<Book> userBooks = new ArrayList<>();
-        for (int i = 0; i < userBooksIndex.size(); i += 1) {
-            int currentBookIndex = userBooksIndex.get(i);
-            userBooks.add(listOfBooks.get(currentBookIndex - 1));
+        for (int i = 0; i < this.userBooksIndex.size(); i += 1) {
+            int currentBookIndex = this.userBooksIndex.get(i);
+            userBooks.add(this.listOfBooks.get(currentBookIndex - 1));
         }
 
         return userBooks;
@@ -51,13 +52,21 @@ public class User {
     public void unborrowBook(Integer bookIndex) {
         this.userBooksIndex.remove(bookIndex);
         UserBook bookToReturn = null;
-        for (int i = 0; i < listOfUserBooks.size(); i += 1) {
-            UserBook currentBook = listOfUserBooks.get(i);
+        for (int i = 0; i < this.listOfUserBooks.size(); i += 1) {
+            UserBook currentBook = this.listOfUserBooks.get(i);
             if (Objects.equals(currentBook.getUserBookIndex(), bookIndex)) {
                 bookToReturn = currentBook;
             }
         }
         this.listOfUserBooks.remove(bookToReturn);
+    }
+
+    public void extendDueDate(Integer bookIndex) {
+        for (int i = 0; i < this.listOfUserBooks.size(); i += 1) {
+            UserBook currentUserBook = this.listOfUserBooks.get(i);
+            LocalDate currentReturnDueDate = currentUserBook.getReturnDueDate();
+            currentUserBook.setReturnDueDate(currentReturnDueDate.plusDays(EXTENSION_DAYS));
+        }
     }
 
     public void setListOfBooks(ArrayList<Book> listOfBooks) {
@@ -66,7 +75,12 @@ public class User {
 
     @Override
     public String toString() {
-        return (this.description + getUserBooks());
+        return (this.userName + getUserBooks());
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        User otherUser = (User) obj;
+        return Objects.equals(this.userName, otherUser.getName());
+    }
 }
