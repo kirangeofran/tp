815cb20797ea7b5b6b57423df6dfcc38f9f2596d
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-04-12 16:42:07 +0800

    Allow user to store borrow and return due date

--- src/main/java/bookmarked/user/User.java
+++ src/main/java/bookmarked/user/User.java
@@ -1,18 +1,23 @@
 package bookmarked.user;
 
 import bookmarked.Book;
+import bookmarked.userBook.UserBook;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class User {
     private String description;
     private ArrayList<Book> listOfBooks;
+    private ArrayList<UserBook> listOfUserBooks;
     private ArrayList<Integer> userBooksIndex;
 
     public User(String description, ArrayList<Book> listOfBooks) {
         this.description = description;
         this.userBooksIndex = new ArrayList<>();
         this.listOfBooks = listOfBooks;
+        this.listOfUserBooks = new ArrayList<>();
     }
 
     public String getName() {
@@ -33,11 +38,26 @@ public class User {
         return this.userBooksIndex;
     }
 
-    public void borrowedBook(Integer bookIndex) {
-        this.userBooksIndex.add(bookIndex);
+    public ArrayList<UserBook> getListOfUserBooks() {
+        return this.listOfUserBooks;
     }
+
+    public void borrowBook(Integer bookIndex, LocalDate borrowDate, LocalDate returnDueDate) {
+        this.userBooksIndex.add(bookIndex);
+        UserBook bookToBorrow = new UserBook(bookIndex, borrowDate, returnDueDate);
+        this.listOfUserBooks.add(bookToBorrow);
+    }
+
     public void unborrowBook(Integer bookIndex) {
         this.userBooksIndex.remove(bookIndex);
+        UserBook bookToReturn = null;
+        for (int i = 0; i < listOfUserBooks.size(); i += 1) {
+            UserBook currentBook = listOfUserBooks.get(i);
+            if (Objects.equals(currentBook.getUserBookIndex(), bookIndex)) {
+                bookToReturn = currentBook;
+            }
+        }
+        this.listOfUserBooks.remove(bookToReturn);
     }
 
     public void setListOfBooks(ArrayList<Book> listOfBooks) {
