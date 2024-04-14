bbca184e417c7d9c53c4e09c4dcc61ed9db1847f
cayennegoh <cayennegoh@gmail.com>
2024-04-14 23:56:39 +0800

    update UG for listcommand

--- src/test/java/bookmarked/command/FindUserCommandTest.java
+++ src/test/java/bookmarked/command/FindUserCommandTest.java
@@ -1,11 +1,15 @@
 package bookmarked.command;
 
+import bookmarked.Book;
 import bookmarked.exceptions.EmptyUserListException;
 import bookmarked.user.User;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FindUserCommandTest {
@@ -17,5 +21,16 @@ public class FindUserCommandTest {
         assertThrows(EmptyUserListException.class, findUser::printUsers);
     }
 
+    @Test
+    public void findUserCommand_userNotFound() throws EmptyUserListException {
+        ByteArrayOutputStream outputWord = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputWord));
+        ArrayList<Book> listOfBooks = new ArrayList<>();
+        ArrayList<User> userList = new ArrayList<>();
+        User a = new User("Tom", listOfBooks);
+        userList.add(a);
+        FindUserCommand findUserCommand = new FindUserCommand(userList, "Tim");
+        findUserCommand.printUsers();
+        assertEquals("user not found", outputWord.toString().trim());
+    }
 }
-
